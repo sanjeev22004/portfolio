a718a6cd4d91bd5375d45f61e8ef02d65706896c
sanjeev prasad <sanjeevprasad2004@gmail.com>
2024-02-10 12:13:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let tabLinks=document.querySelectorAll('.tab-links');
+
+tabLinks.forEach(tab=>{
+    tab.addEventListener('click',()=>{
+        //remove active link
+        tabLinks.forEach(link=>link.classList.remove('active-link'));
+        document.querySelectorAll('.tab-content').forEach(content=>content.classList.remove('active-tab'));
+        //add active link
+        tab.classList.add('active-link');
+        let index=Array.from(tabLinks).indexOf(tab);
+
+        document.querySelectorAll('.tab-content')[index].classList.add('active-tab');
+    })
+})
