08d300947e88ce266be80e24c79fe5285074e290
sanjeev prasad <sanjeevprasad2004@gmail.com>
2024-02-10 22:28:58 +0530

    upadated css

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-let tabLinks=document.querySelectorAll('.tab-links');
+document.addEventListener('DOMContentLoaded', function () {
+    let tabLinks = document.querySelectorAll('.tab-links');
 
-tabLinks.forEach(tab=>{
-    tab.addEventListener('click',()=>{
-        //remove active link
-        tabLinks.forEach(link=>link.classList.remove('active-link'));
-        document.querySelectorAll('.tab-content').forEach(content=>content.classList.remove('active-tab'));
-        //add active link
-        tab.classList.add('active-link');
-        let index=Array.from(tabLinks).indexOf(tab);
+    tabLinks.forEach(tab => {
+        tab.addEventListener('click', () => {
+            // remove active link
+            tabLinks.forEach(link => link.classList.remove('active-link'));
+            document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active-tab'));
+            // add active link
+            tab.classList.add('active-link');
+            let index = Array.from(tabLinks).indexOf(tab);
 
-        document.querySelectorAll('.tab-content')[index].classList.add('active-tab');
-    })
-})
+            document.querySelectorAll('.tab-content')[index].classList.add('active-tab');
+        })
+    });
+});
